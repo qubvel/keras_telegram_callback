44d78f6ea290fca3ccde55494b85bf316a813991
Pavel Yakubovskiy <pavelyakubovskiy@ubuntu.local>
2018-04-24 00:50:44 +0300

    telegram callback

--- callbacks/telegram_callback.py
+++ callbacks/telegram_callback.py
@@ -5,7 +5,7 @@ from keras.callbacks import Callback
 class TelegramCallback(Callback):
 
     def __init__(self, config):
-        super(TelegramBot, self).__init__()
+        super(TelegramCallback, self).__init__()
         self.user_id = config['telegram_id']
         self.bot = telegram.Bot(config['token'])
 
@@ -13,14 +13,14 @@ class TelegramCallback(Callback):
         try:
             self.bot.send_message(chat_id=self.user_id, text=text)
         except Exception as e:
-            print ('Messege did not send. Error: {}.',fromat(e))
+            print('Message did not send. Error: {}.'.format(e))
 
     def on_train_begin(self, logs={}):
         text = 'Start training model {}.'.format(self.model.name)
         self.send_message(text)
 
     def on_epoch_end(self, epoch, logs={}):
-        text = 'Epoch {}.\\n'.format(epoch)
+        text = 'Epoch {}.\n'.format(epoch)
         for k, v in logs.items():
-            text += '{}: {:.4f}; '
+            text += '{}: {:.4f}; '.format(k, v)
         self.send_message(text)
