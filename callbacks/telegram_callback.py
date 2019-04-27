3c4b0fa079fa676c7ae712015700a992b9bb5460
mosheliv <mosheliv@users.noreply.github.com>
2019-04-28 06:23:44 +1200

    make training multiple models in parallel clearer, more precision for lr (#1)

--- callbacks/telegram_callback.py
+++ callbacks/telegram_callback.py
@@ -4,10 +4,14 @@ from keras.callbacks import Callback
 
 class TelegramCallback(Callback):
 
-    def __init__(self, config):
+    def __init__(self, config, name=None):
         super(TelegramCallback, self).__init__()
         self.user_id = config['telegram_id']
         self.bot = telegram.Bot(config['token'])
+        if name != None:
+            self.name = name
+        else:
+            self.name = self.model.name
 
     def send_message(self, text):
         try:
@@ -16,11 +20,18 @@ class TelegramCallback(Callback):
             print('Message did not send. Error: {}.'.format(e))
 
     def on_train_begin(self, logs={}):
-        text = 'Start training model {}.'.format(self.model.name)
+        text = 'Start training model {}.'.format(self.name)
+        self.send_message(text)
+
+    def on_train_end(self, logs={}):
+        text = 'Training model {} ended.'.format(self.name)
         self.send_message(text)
 
     def on_epoch_end(self, epoch, logs={}):
-        text = 'Epoch {}.\n'.format(epoch)
+        text = '{}: Epoch {}.\n'.format(self.name, epoch)
         for k, v in logs.items():
-            text += '{}: {:.4f}; '.format(k, v)
+            if k != "lr":
+                text += '{}: {:.4f}; '.format(k, v)
+            else:
+                text += '{}: {:.6f}; '.format(k, v) #4 decimal places too short for learning rate
         self.send_message(text)
